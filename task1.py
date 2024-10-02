fbfb857216c77918408b387ae6cf15101bf5c5cf
Alexsandr2008 <138139879+Alexsandr2008@users.noreply.github.com>
2024-10-02 14:33:53 +0000

    1 работа

--- task1.py
+++ task1.py
@@ -1,5 +1,6 @@
-print("Hello, World!")
-print("Маша + Петя = Любовь")
-print("x = 3 + 4")
-a = 3 + 4
-print("x =" a)
+print ("Hello World!")
+print ("Маша + Петя = Любовь")
+x = "3 + 4"
+print ("x = ",  x)
+x = 3 + 4
+print ("x = ",  x)
--- task2.py
+++ task2.py
@@ -1,2 +1,8 @@
-type(x = "Hello World" )
+x = "Hello World!"
+print (type(x))
+x = 3 + 4
+print (type(x))
+x = 3/4 
+print(type(x))
+x = [1,2,5,10,100]
 print(type(x))
