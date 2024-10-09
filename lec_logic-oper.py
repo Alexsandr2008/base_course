a05a65f6ce789c8172bfb6b1beed0fc24479f146
Alexsandr2008 <138139879+Alexsandr2008@users.noreply.github.com>
2024-10-09 14:14:22 +0000

    2 урок

--- lec_logic-oper.py
+++ lec_logic-oper.py
@@ -2,7 +2,7 @@ a = 3
 b = 4
 if a > 4 and b == 2: # and - операция логического И
     print ("Good")
-elif b > 3 or c == 5: # or - операция логоческого или
+elif b > 3 or a  == 5: # or - операция логоческого или
     print ("Best")
 else : 
     print ("Bad")
